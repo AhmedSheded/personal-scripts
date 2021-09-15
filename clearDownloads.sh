bd028f1467a6088180fd851e614fa70032e4d931
Ahmed Sheded <ahmed.shededmmm@gmail.com>
2021-09-15 18:15:00 +0200

    firstd

---
+++ clearDownloads.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CWD="$(pwd)"
+cd /home/sheded/Downloads
+mv *.doc *.pdf *.pptx *.epub *.odt *.docx /home/sheded/Documents/Downloads
+mv *.mp4 *.mkv *.webm /home/sheded/Videos/Downloads
+mv *.exe *.zip *.jar *.run *.AppImage *.deb *.save *.rar *.sh *.bin *.gz *.asm *.java *.html *.css *.js /home/sheded/packages
+mv *.torrent /home/sheded/torrent
+mv *.csv *.txt *.py *.ipynb /home/sheded/files
+mv *.mp3 *.mpga /home/sheded/Music/Downloads
+mv *.jpg *.svg *.png *.jpeg *.svgz /home/sheded/Pictures/Downloads
+mv *.gif *.webp /home/sheded/Pictures/Gifs
---
+++ deactivate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+CWD="$(pwd)"
+python3 /home/sheded/scripts/py/deactivate.py
